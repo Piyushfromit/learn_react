4454154563f025b6d070fdcc6ebe5fc9aaac66fd
piyush_kumar <piyush.kumar@sbsind.in>
2024-05-06 13:45:01 +0530

    kjdhdk

--- react3/react3/src/App.js
+++ react3/react3/src/App.js
@@ -1,15 +1,23 @@
 
+import React from 'react';
+
 
 
 function App() {
+  const [count, setCount] = React.useState(0);
+  // count is Curent count value
+  // setCount is update function / dispacher function, which will update the count
   return (
     <div className="App">
-         Piyush Kumar  
-       
-         
-     
+         <h1>Count: {count}</h1>
+        <button onClick={() => setCount(count + 1)}>INC</button>
+        <button onClick={() => setCount(count - 1)}>dec</button>
+        <button onClick={() => setCount(0)}>Reset</button>
     </div>
   );
 }
 
 export default App;
+
+
+// lecture 3, 00:50

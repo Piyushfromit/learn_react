e0f7b968e6b49f18d920d3cf36bb4df85e37d0a9
piyushfromit <piyushfrommasai@gmail.com>
2025-09-03 00:59:41 +0530

    learming react by chai and code

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,23 +1,27 @@
-import React from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
 
+
 function MyApp(){
+
+     const userName = "Hi i am Piyush!";
+
      return(
       <div>
-        <h1>Custom MyApp!</h1>
+        <h1>Custom MyApp! {userName}</h1>
       </div>
      )
 }
 
-// createRoot(document.getElementById('root')).render(
-  
-//   <StrictMode>
-//     <App />
-//     <MyApp />
-//   </StrictMode>
-// )
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+    <MyApp />
+  </StrictMode>
+)
 
 
 // const reactElement = {
@@ -38,14 +42,14 @@ function MyApp(){
 //     anotherElement
 // )
 
-const reactElement = React.createElement(
-   'a',
-   { href: 'https://google.com', target:'_blank' },
-   'click me to visit google'
-)
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  
-     reactElement
 
-)
+// const reactElement = React.createElement(
+//   'a',
+//   { href: 'https://google.com', target: '_blank' },
+//   'click me to visit google'
+// )
+
+// createRoot(document.getElementById('root')).render(
+//   reactElement
+// )

3c7a964b22f34e9f60d6d4e80ecd95124e26d73f
piyush_kumar <piyush.kumar@sbsind.in>
2025-09-04 19:00:05 +0530

    telwinn css

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -8,26 +8,31 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    
+      <h1 className ="bg-green-400 text-3xl font-bold underline mb-4" >Tailwind Test</h1>
+
+
+      <div className="w-60 flex flex-col rounded-xl bg-black min-h-[19rem] ">
+        <div>
+          <img
+            src="https://cdn.vox-cdn.com/thumbor/ZkmdkuJUTLgJh96_FWQ5zweGGxo=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/23084330/bored_ape_nft_accidental_.jpg"
+            alt="test"
+            className="object-cover object-center rounded-t-xl"
+          />
+        </div>
+        <div className="flex flex-col py-3 px-3 pb-10">
+          <div className="flex justify-between ">
+            <p className="text-white  text-sm">Bored ape nft accidental</p>
+            <p className="text-white  text-sm">Price</p>
+          </div>
+          <div className="flex  justify-between">
+            <p className="text-white text-sm" >#345</p>
+            <p className="text-white text-sm" >0.01</p>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
+      
     </>
   )
 }

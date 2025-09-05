940a3146dc1141ca80126409a69a7ca0d96d929b
piyush_kumar <piyush.kumar@sbsind.in>
2025-09-05 19:43:17 +0530

    learning react

--- 04bgchanger/src/App.jsx
+++ 04bgchanger/src/App.jsx
@@ -4,9 +4,32 @@ import './App.css'
 
 function App() {
 
+  const [color, setColor] = useState("olive")
   return (
     <>
-      <h1>Piyush</h1>
+      <div className="W-Full h-screen duration-200" style = {{background: color}}   >
+          
+        <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+            
+          <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+               
+            <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style = {{backgroundColor: "red"}}>Red</button>
+            <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style = {{backgroundColor: "green"}}>Green</button>
+            <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style = {{backgroundColor: "blue"}}>Blue</button>
+            <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style = {{backgroundColor: "olive"}}>Olive</button>
+            <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style = {{backgroundColor: "gray"}}>Gray</button>
+            <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style = {{backgroundColor: "yellow"}}>Yellow</button>
+            <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style = {{backgroundColor: "pink"}}>Pink</button>
+            <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style = {{backgroundColor: "purple"}}>Purple</button>
+            <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style = {{backgroundColor: "lavender"}}>Lavender</button>
+            <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style = {{backgroundColor: "white"}}>White</button>
+            <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style = {{backgroundColor: "black"}}>Black</button>
+          
+          </div>
+
+        </div>
+
+      </div>
     </>
   )
 }

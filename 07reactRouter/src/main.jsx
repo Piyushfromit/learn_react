7c4d05d3bc3b98cd233a38c47c87542fc3b73031
piyush_kumar <piyush.kumar@sbsind.in>
2025-09-19 19:10:49 +0530

    07

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -2,10 +2,11 @@ import { Children, StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { Route, RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Layout from './Layout.jsx'
 import About from './components/About/About.jsx'
 import Home from './components/Home/Home.jsx'
+import Contact from './components/Contact/Contact.jsx'
 
 
 
@@ -21,10 +22,31 @@ const router = createBrowserRouter([
       {
         path: "about",
         element: <About />
+      },
+      {
+        path: "contact",
+        element: <Contact />
       }
     ]
   }
 ])
+
+
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path='/' element={<Layout />}>
+//       <Route path='' element={<Home />} />
+//       <Route path='about' element={<About />} />
+//       <Route path='contact' element={<Contact />} />
+//       <Route path='user/:userid' element={<User />} />
+//       <Route 
+//       loader={githubInfoLoader}
+//       path='github' 
+//       element={<Github />}
+//        />
+//     </Route>
+//   )
+// )
 
 
 createRoot(document.getElementById('root')).render(
